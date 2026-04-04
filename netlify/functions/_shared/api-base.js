7e62f2dc62a869d437fac198f5a6ad34e5d05db2
paul <paul@webwing.agency>
2026-04-04 19:02:31 +0200

    delete netlify functions

--- netlify/functions/_shared/api-base.js
+++
@@ -1,3 +0,0 @@
-// netlify/functions/_shared/api-base.js
-// kleine Hilfsdatei für Functions: liefert die Basis-URL, serverseitig aus env
-export const API_BASE = (process.env.BOOKING_API_BASE || process.env.URL || 'http://localhost:8888').replace(/\/$/, '');
